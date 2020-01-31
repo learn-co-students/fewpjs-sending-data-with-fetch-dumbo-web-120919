7bbf45c46affcfb7ec75b470a590718aeb4cba2f
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-01-31 12:18:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+const pageBody = document.body;
+
+function submitData(userName, userEmail) {
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail
+    })
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    pageBody.append(json.id);
+  })
+  .catch(function(error) {
+    pageBody.append(error.message);
+  })
+};
